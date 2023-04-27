11e069dd3a0e767f9ab2d60bff7f8daee2b88af0
Hardy Griech <ntbox@gmx.net>
2023-04-27 12:42:57 +0200

    create librtt.a

--- contrib/contrib.cmake
+++ contrib/contrib.cmake
@@ -2,6 +2,7 @@ OPTION(LLVM_TOOLCHAIN_CONTRIB_PROFILE "Profiling Support" Off)
 
 
 include(contrib/profiling/clang_rt.profile.cmake)
+include(contrib/rtt/rtt.cmake)
 
 
 function(add_contrib_libs directory variant target_triple flags libc_target)
@@ -9,5 +10,8 @@ function(add_contrib_libs directory variant target_triple flags libc_target)
     
     if(LLVM_TOOLCHAIN_CONTRIB_PROFILE)
         add_contrib_lib_profile(${directory} ${variant} ${target_triple} ${flags} ${libc_target})
+    endif()
+    if(LLVM_TOOLCHAIN_CONTRIB_RTT)
+        add_contrib_lib_rtt(${directory} ${variant} ${target_triple} ${flags} ${libc_target})
     endif()
 endfunction()
