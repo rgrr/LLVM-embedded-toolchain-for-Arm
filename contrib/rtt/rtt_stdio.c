b7e49dd5f11b1caf214cdb28cdc70102580380ca
Hardy Griech <ntbox@gmx.net>
2023-07-20 16:53:14 +0200

    rtt_sysview

--- contrib/rtt/rtt_stdio.c
+++ contrib/rtt/rtt_stdio.c
@@ -1,4 +1,4 @@
-/*===- rtt_stdio.c - Write instrumentation to a file --------===*\
+/*===- rtt_stdio.c - redirect printf et al to RTT--------------------------===*\
 |*
 |* Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 |* See https://llvm.org/LICENSE.txt for license information.
