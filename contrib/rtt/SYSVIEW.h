04d3114a4d0feb1b6dfb62eb089c1d9e5f6bd086
Hardy Griech <ntbox@gmx.net>
2023-07-22 21:48:47 +0200

    "High level" API for SysView which allows easy "NoOS" monitoring

---
+++ contrib/rtt/SYSVIEW.h
@@ -0,0 +1,146 @@
+/*===- SYSVIEW.h - Segger SysView API header---------------------- --------===*\
+|*
+|* Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+|* See https://llvm.org/LICENSE.txt for license information.
+|* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+|*
+\*===----------------------------------------------------------------------===*/
+
+
+
+#ifndef __SYSVIEW_H
+#define __SYSVIEW_H
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+
+#ifndef SEGGER_SYSVIEW_OFF
+
+    #include "SEGGER_RTT.h"
+    #include "SEGGER_SYSVIEW.h"
+
+    // some "high level" SystemView API
+    void SYSVIEW_SetSystem(const char *appName, const char *deviceName, const char *coreName, const char *osName);
+    void SYSVIEW_Init(uint32_t sysFreq, uint32_t cpuFreq, uint64_t (*getTime)(void));
+    void SYSVIEW_ConfigUpBuffer(void* pBuffer, unsigned BufferSize);
+
+    void SYSVIEW_AddTask(uint32_t taskId, const char *name);
+    void SYSVIEW_AddMarker(uint32_t markerId, const char *name);
+    void SYSVIEW_AddInt(uint32_t intId, const char *name);
+
+#else
+
+    #define SYSVIEW_SetSystem(...)
+    #define SYSVIEW_Init(...)
+    #define SYSVIEW_ConfigUpBuffer(...)
+
+    #define SYSVIEW_AddTask(...)
+    #define SYSVIEW_AddMarker(...)
+    #define SYSVIEW_AddInt(...)
+
+    #define SEGGER_SYSVIEW_Init(...)
+    #define SEGGER_SYSVIEW_SetRAMBase(...)
+    #define SEGGER_SYSVIEW_Start(...)                    
+    #define SEGGER_SYSVIEW_Stop(...)                     
+    #define SEGGER_SYSVIEW_GetSysDesc(...)               
+    #define SEGGER_SYSVIEW_SendTaskList(...)             
+    #define SEGGER_SYSVIEW_SendTaskInfo(...)             
+    #define SEGGER_SYSVIEW_SendSysDesc(...)              
+    #define SEGGER_SYSVIEW_IsStarted(...)                
+    #define SEGGER_SYSVIEW_GetChannelID(...)             
+ 
+    #define SEGGER_SYSVIEW_RecordVoid(...)               
+    #define SEGGER_SYSVIEW_RecordU32(...)                
+    #define SEGGER_SYSVIEW_RecordU32x2(...)              
+    #define SEGGER_SYSVIEW_RecordU32x3(...)              
+    #define SEGGER_SYSVIEW_RecordU32x4(...)              
+    #define SEGGER_SYSVIEW_RecordU32x5(...)              
+    #define SEGGER_SYSVIEW_RecordU32x6(...)              
+    #define SEGGER_SYSVIEW_RecordU32x7(...)              
+    #define SEGGER_SYSVIEW_RecordU32x8(...)              
+    #define SEGGER_SYSVIEW_RecordU32x9(...)              
+    #define SEGGER_SYSVIEW_RecordU32x10(...)             
+    #define SEGGER_SYSVIEW_RecordString(...)             
+    #define SEGGER_SYSVIEW_RecordSystime(...)            
+    #define SEGGER_SYSVIEW_RecordEnterISR(...)           
+    #define SEGGER_SYSVIEW_RecordExitISR(...)            
+    #define SEGGER_SYSVIEW_RecordExitISRToScheduler(...) 
+    #define SEGGER_SYSVIEW_RecordEnterTimer(...)         
+    #define SEGGER_SYSVIEW_RecordExitTimer(...)          
+    #define SEGGER_SYSVIEW_RecordEndCall(...)            
+    #define SEGGER_SYSVIEW_RecordEndCallU32(...)         
+
+    #define SEGGER_SYSVIEW_OnIdle(...)                   
+    #define SEGGER_SYSVIEW_OnTaskCreate(...)             
+    #define SEGGER_SYSVIEW_OnTaskTerminate(...)          
+    #define SEGGER_SYSVIEW_OnTaskStartExec(...)          
+    #define SEGGER_SYSVIEW_OnTaskStopExec(...)           
+    #define SEGGER_SYSVIEW_OnTaskStartReady(...)         
+    #define SEGGER_SYSVIEW_OnTaskStopReady(...)          
+    #define SEGGER_SYSVIEW_MarkStart(...)                
+    #define SEGGER_SYSVIEW_MarkStop(...)                 
+    #define SEGGER_SYSVIEW_Mark(...)                     
+    #define SEGGER_SYSVIEW_NameMarker(...)               
+
+    #define SEGGER_SYSVIEW_HeapDefine(...)               
+    #define SEGGER_SYSVIEW_HeapAlloc(...)                
+    #define SEGGER_SYSVIEW_HeapAllocEx(...)              
+    #define SEGGER_SYSVIEW_HeapFree(...)                 
+
+    #define SEGGER_SYSVIEW_NameResource(...)             
+
+    #define SEGGER_SYSVIEW_SendPacket(...)               
+
+    #define SEGGER_SYSVIEW_EncodeU32(...)                
+    #define SEGGER_SYSVIEW_EncodeData(...)               
+    #define SEGGER_SYSVIEW_EncodeString(...)             
+    #define SEGGER_SYSVIEW_EncodeId(...)                 
+    #define SEGGER_SYSVIEW_ShrinkId(...)                 
+
+    #define SEGGER_SYSVIEW_RegisterModule(...)           
+    #define SEGGER_SYSVIEW_RecordModuleDescription(...)  
+    #define SEGGER_SYSVIEW_SendModule(...)               
+    #define SEGGER_SYSVIEW_SendModuleDescription(...)    
+    #define SEGGER_SYSVIEW_SendNumModules(...)           
+
+    #define SEGGER_SYSVIEW_PrintfHostEx(...)             
+    #define SEGGER_SYSVIEW_VPrintfHostEx(...)            
+    #define SEGGER_SYSVIEW_PrintfTargetEx(...)          
+    #define SEGGER_SYSVIEW_VPrintfTargetEx(...)          
+    #define SEGGER_SYSVIEW_PrintfHost(...)               
+    #define SEGGER_SYSVIEW_VPrintfHost(...)              
+    #define SEGGER_SYSVIEW_PrintfTarget(...)             
+    #define SEGGER_SYSVIEW_VPrintfTarget(...)            
+    #define SEGGER_SYSVIEW_WarnfHost(...)                
+    #define SEGGER_SYSVIEW_VWarnfHost(...)               
+    #define SEGGER_SYSVIEW_WarnfTarget(...)              
+    #define SEGGER_SYSVIEW_VWarnfTarget(...)             
+    #define SEGGER_SYSVIEW_ErrorfHost(...)               
+    #define SEGGER_SYSVIEW_VErrorfHost(...)              
+    #define SEGGER_SYSVIEW_ErrorfTarget(...)             
+    #define SEGGER_SYSVIEW_VErrorfTarget(...)            
+
+    #define SEGGER_SYSVIEW_Print(...)                    
+    #define SEGGER_SYSVIEW_Warn(...)                     
+    #define SEGGER_SYSVIEW_Error(...)                    
+
+    #define SEGGER_SYSVIEW_EnableEvents(...)             
+    #define SEGGER_SYSVIEW_DisableEvents(...)            
+
+    #define SEGGER_SYSVIEW_Conf(...)                     
+    #define SEGGER_SYSVIEW_X_GetTimestamp(...)           
+    #define SEGGER_SYSVIEW_X_GetInterruptId(...)         
+
+    #define SEGGER_SYSVIEW_X_StartComm(...)              
+    #define SEGGER_SYSVIEW_X_OnEventRecorded(...)       
+
+#endif
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif
--- contrib/rtt/rtt_sysview.c
+++ contrib/rtt/rtt_sysview.c
@@ -7,47 +7,191 @@
 \*===----------------------------------------------------------------------===*/
 
 
+#include "SYSVIEW.h"
+#include <stdio.h>
+#include <string.h>
 
-#include "SEGGER_SYSVIEW.h"
+
+typedef struct {
+    uint32_t    id;
+    const char *name;
+} SYSVIEW_INFO;
+
+
+#define NUM_TASKS    32
+static SYSVIEW_INFO  _aTasks[NUM_TASKS];
+static int           _NumTasks;
+
+#define NUM_MARKER   16
+static SYSVIEW_INFO  _aMarker[NUM_MARKER];
+static int           _NumMarker;
+
+#define NUM_INTS     16
+static SYSVIEW_INFO  _aInts[NUM_INTS];
+static int           _NumInts;
+
+static const char  *sysviewAppName;
+static const char  *sysviewDeviceName;
+static const char  *sysviewCoreName;
+static const char  *sysviewOsName;
+
 
 
 #if SEGGER_SYSVIEW_CORE == SEGGER_SYSVIEW_CORE_CM3
 
+    #define DEMCR           (*(volatile unsigned long*) (0xE000EDFCuL))     // Debug Exception and Monitor Control Register
+    #define TRACEENA_BIT    (1uL << 24)                                     // Trace enable bit
 
-#define DEMCR           (*(volatile unsigned long*) (0xE000EDFCuL))     // Debug Exception and Monitor Control Register
-#define TRACEENA_BIT    (1uL << 24)                                     // Trace enable bit
-
-#define DWT_CTRL        (*(volatile unsigned long*) (0xE0001000uL))     // DWT Control Register
-#define NOCYCCNT_BIT    (1uL << 25)                                     // Cycle counter support bit
-#define CYCCNTENA_BIT   (1uL << 0)                                      // Cycle counter enable bit
+    #define DWT_CTRL        (*(volatile unsigned long*) (0xE0001000uL))     // DWT Control Register
+    #define NOCYCCNT_BIT    (1uL << 25)                                     // Cycle counter support bit
+    #define CYCCNTENA_BIT   (1uL << 0)                                      // Cycle counter enable bit
 
 
-static void CycCnt_Init(void)
-{
-    //
-    // If no debugger is connected, the DWT must be enabled by the application
-    //
-    if ((DEMCR & TRACEENA_BIT) == 0) {
-        DEMCR |= TRACEENA_BIT;
-    }
-
-    //
-    //  The cycle counter must be activated in order
-    //  to use time related functions.
-    //
-    if ((DWT_CTRL & NOCYCCNT_BIT) == 0) {                  // Cycle counter supported?
-        if ((DWT_CTRL & CYCCNTENA_BIT) == 0) {             // Cycle counter not enabled?
-            DWT_CTRL |= CYCCNTENA_BIT;                     // Enable Cycle counter
+    static void CycCnt_Init(void)
+    {
+        //
+        // If no debugger is connected, the DWT must be enabled by the application
+        //
+        if ((DEMCR & TRACEENA_BIT) == 0) {
+            DEMCR |= TRACEENA_BIT;
         }
-    }
-}   // CycCnt_Init
+
+        //
+        //  The cycle counter must be activated in order
+        //  to use time related functions.
+        //
+        if ((DWT_CTRL & NOCYCCNT_BIT) == 0) {                  // Cycle counter supported?
+            if ((DWT_CTRL & CYCCNTENA_BIT) == 0) {             // Cycle counter not enabled?
+                DWT_CTRL |= CYCCNTENA_BIT;                     // Enable Cycle counter
+            }
+        }
+    }   // CycCnt_Init
 
 
 
-__attribute__((weak)) void SEGGER_SYSVIEW_Conf(void)
-{
-    CycCnt_Init();
-}   // SEGGER_SYSVIEW_Conf
-
+    __attribute__((weak)) void SEGGER_SYSVIEW_Conf(void)
+    {
+        CycCnt_Init();
+    }   // SEGGER_SYSVIEW_Conf
 
 #endif
+
+
+
+static void _cbSendSystemDesc(void)
+{
+    char buf[128];
+
+    if (sysviewAppName != NULL) {
+        sprintf(buf, "N=%s", sysviewAppName);
+        SEGGER_SYSVIEW_SendSysDesc(buf);
+    }
+    if (sysviewDeviceName != NULL) {
+        sprintf(buf, "D=%s", sysviewDeviceName);
+        SEGGER_SYSVIEW_SendSysDesc(buf);
+    }
+    if (sysviewCoreName != NULL) {
+        sprintf(buf, "C=%s", sysviewCoreName);
+        SEGGER_SYSVIEW_SendSysDesc(buf);
+    }
+    if (sysviewOsName != NULL) {
+        sprintf(buf, "O=%s", sysviewOsName);
+        SEGGER_SYSVIEW_SendSysDesc(buf);
+    }
+
+    for (int n = 0; n < _NumInts; n++) {
+        sprintf(buf, "I#%d=%s", _aInts[n].id, _aInts[n].name);
+        SEGGER_SYSVIEW_SendSysDesc(buf);
+    }
+}   // _cbSendSystemDesc
+
+
+static void _cbSendTaskList(void)
+{
+    for (int n = 0; n < _NumTasks; n++) {
+        SEGGER_SYSVIEW_TASKINFO info;
+
+        memset(&info, 0, sizeof(info));
+        info.TaskID = _aTasks[n].id;
+        info.sName  = _aTasks[n].name;
+        SEGGER_SYSVIEW_SendTaskInfo(&info);
+    }
+    for (int n = 0; n < _NumMarker; n++) {
+        SEGGER_SYSVIEW_NameMarker(_aMarker[n].id, _aMarker[n].name);
+    }
+}   // _cbSendTaskList
+
+
+void SYSVIEW_AddTask(uint32_t taskId, const char *name)
+{
+    int n;
+
+    SEGGER_SYSVIEW_OnTaskCreate(taskId);
+
+    if (_NumTasks > NUM_TASKS) {
+        return;
+    }
+    n = _NumTasks;
+    _NumTasks++;
+
+    _aTasks[n].id   = taskId;
+    _aTasks[n].name = name;
+}   // SYSVIEW_AddTask
+
+
+void SYSVIEW_AddMarker(uint32_t markerId, const char *name)
+{
+    int n;
+
+    if (_NumMarker > NUM_MARKER) {
+        return;
+    }
+    n = _NumMarker;
+    _NumMarker++;
+
+    _aMarker[n].id   = markerId;
+    _aMarker[n].name = name;
+}   // SYSVIEW_AddMarker
+
+
+void SYSVIEW_AddInt(uint32_t intId, const char *name)
+{
+    int n;
+
+    if (_NumInts > NUM_INTS) {
+        return;
+    }
+    n = _NumInts;
+    _NumInts++;
+
+    _aInts[n].id   = intId;
+    _aInts[n].name = name;
+}   // SYSVIEW_AddInt
+
+
+void SYSVIEW_SetSystem(const char *appName, const char *deviceName, const char *coreName, const char *osName)
+/**
+ * Function descriptions go to /opt/SEGGER/SystemView_V<version>/Description/SYSVIEW_<osName>.txt
+ */
+{
+    sysviewAppName    = appName;
+    sysviewDeviceName = deviceName;
+    sysviewCoreName   = coreName;
+    sysviewOsName     = osName;
+}   // SYSVIEW_SetSystem
+
+
+void SYSVIEW_Init(uint32_t sysFreq, uint32_t cpuFreq, uint64_t (*getTime)(void))
+{
+    static SEGGER_SYSVIEW_OS_API osAPI = {NULL, _cbSendTaskList};
+
+    osAPI.pfGetTime = getTime;
+    SEGGER_SYSVIEW_Conf();
+    SEGGER_SYSVIEW_Init(sysFreq, cpuFreq, &osAPI, _cbSendSystemDesc);
+}   // SYSVIEW_Init
+
+
+void SYSVIEW_ConfigUpBuffer(void* pBuffer, unsigned BufferSize)
+{
+    SEGGER_RTT_ConfigUpBuffer(SEGGER_SYSVIEW_RTT_CHANNEL, "SysView", pBuffer, BufferSize, SEGGER_RTT_MODE_NO_BLOCK_SKIP);
+}   // SYSVIEW_ConfigUpBuffer
